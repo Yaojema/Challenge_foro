fc17e32fcab7d1f16ef7f6ee8aeff68fe2cb3627
Yaojema <yaojema@gmail.com>
2024-07-13 11:18:26 -0500

    ➕:ADD - Se transforma la entidad Usuario a UserDetails implmentando sus metodos, se define el PasswordEncoder

--- src/main/java/com/challenge/foro/controller/AutenticacionController.java
+++ src/main/java/com/challenge/foro/controller/AutenticacionController.java
@@ -1,12 +1,14 @@
 package com.challenge.foro.controller;
 
 import com.challenge.foro.domain.usuarios.DatosAutenticacionUsuario;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,7 @@ public class AutenticacionController {
     private AuthenticationManager authenticationManager;
 
     @PostMapping
-    public ResponseEntity autenticacionUsuario(DatosAutenticacionUsuario datosAutenticacionUsuario){
+    public ResponseEntity autenticacionUsuario(@RequestBody @Valid DatosAutenticacionUsuario datosAutenticacionUsuario){
         Authentication token = new UsernamePasswordAuthenticationToken(
                 datosAutenticacionUsuario.login(),
                 datosAutenticacionUsuario.clave());
