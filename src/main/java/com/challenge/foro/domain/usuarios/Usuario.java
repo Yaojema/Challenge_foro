18de40e33a5a02b43aea82956c64111e3de3d196
Yaojema <yaojema@gmail.com>
2024-07-13 09:13:06 -0500

    ➕:ADD - Se agrega las dependencias de Security, se crea la entidad Usuario y se hace una migracion para table usuarios

---
+++ src/main/java/com/challenge/foro/domain/usuarios/Usuario.java
@@ -0,0 +1,20 @@
+package com.challenge.foro.domain.usuarios;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String clave;
+}
